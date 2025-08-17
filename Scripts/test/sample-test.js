7a764045cd62eeb21af5bc8585976eb5f1207cd9
Sandeep2006 <sandeeppradhan900@gmail.com>
2025-08-17 12:43:56 +0530

    Created 'test.html' using Jasmine. Deleted the default spec and src files.

--- Scripts/test/sample-test.js
+++ Scripts/test/sample-test.js
@@ -1,5 +1,15 @@
 import { format } from "../Utils/currencyformatter.js"
 
+
+console.log("Test Suite: formatCurrency");
+
+console.log("Converts cents to dollars")
+if (format(2005)==='20.05')
+    console.log('passed');
+else 
+    console.log('failed')
+
+console.log("Rounding off to nearest decimal")
 if (format(207.5)==='2.08')
     console.log("passed");
 else 
