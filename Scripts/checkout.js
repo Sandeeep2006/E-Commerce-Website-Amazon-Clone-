6a6c45edc2522c99f5194e91e460059f921a26c9
Sandeep2006 <sandeeppradhan900@gmail.com>
2025-08-22 02:08:39 +0530

    Changed Promises to async await

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -2,20 +2,35 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend.js';
-import { loadProducts,loadProductsFetch } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
-Promise.all([
-    loadProductsFetch(),
-    new Promise((resolve)=>{
+
+async function loadPage(){
+
+    await loadProductsFetch();
+    const value =await new Promise((resolve)=>{
         loadCart(()=>{
-            resolve();
+            resolve('value3');
         });
-    })
-]).then(()=>{
+    });
     renderOrderSummary();
     renderPaymentSummary();
-});
+
+}
+loadPage();
+
+// Promise.all([
+//     loadProductsFetch(),
+//     new Promise((resolve)=>{
+//         loadCart(()=>{
+//             resolve();
+//         });
+//     })
+// ]).then(()=>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
 
 // new Promise((resolve)=>{
 //     loadProducts(()=>{
